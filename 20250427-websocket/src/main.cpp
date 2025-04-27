efea52fb25e20978653171ba3901d1ce16927764
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-04-27 05:15:49 +0000

    WIP

--- 20250427-websocket/src/main.cpp
+++ 20250427-websocket/src/main.cpp
@@ -5,26 +5,37 @@
 #include <WiFiClientSecure.h>
 
 #include <WebSocketsServer.h>
+#include <Adafruit_NeoPixel.h>
 
 #include <wifi_settings.hpp>
 
+#define CH9329_TX_PIN GPIO_NUM_7
+#define CH9329_RX_PIN GPIO_NUM_8
+#define LED_PIN GPIO_NUM_2
+
+#define LED_COLOR_SETUP Adafruit_NeoPixel::Color(0, 32, 0)
+#define LED_COLOR_NORMAL Adafruit_NeoPixel::Color(0, 0, 32)
+#define LED_COLOR_CONNECT Adafruit_NeoPixel::Color(32, 32, 0)
+#define LED_COLOR_OPERATION Adafruit_NeoPixel::Color(0, 32, 32)
+
 WiFiMulti wifiMulti;
 WebSocketsServer webSocket = WebSocketsServer(81);
+Adafruit_NeoPixel pixels(1, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void hexdump(const void *mem, uint32_t len, uint8_t cols = 16)
 {
   const uint8_t *src = (const uint8_t *)mem;
-  Serial.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
+  Serial0.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
   for (uint32_t i = 0; i < len; i++)
   {
     if (i % cols == 0)
     {
-      Serial.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
+      Serial0.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
     }
-    Serial.printf("%02X ", *src);
+    Serial0.printf("%02X ", *src);
     src++;
   }
-  Serial.printf("\n");
+  Serial0.printf("\n");
 }
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
@@ -33,24 +44,42 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
   switch (type)
   {
   case WStype_DISCONNECTED:
-    Serial.printf("[%u] Disconnected!\n", num);
+    Serial0.printf("[%u] Disconnected!\n", num);
+    pixels.setPixelColor(0, LED_COLOR_NORMAL);
+    pixels.show();
     break;
   case WStype_CONNECTED:
   {
     IPAddress ip = webSocket.remoteIP(num);
-    Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+    Serial0.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
 
-    // send message to client
     webSocket.sendTXT(num, "Connected");
+    pixels.setPixelColor(0, LED_COLOR_CONNECT);
+    pixels.show();
   }
   break;
-  case WStype_TEXT:
-    break;
+
   case WStype_BIN:
-    Serial.printf("[%u] get binary length: %u\n", num, length);
+#ifdef ENABLE_DEBUG_PRINT
+    Serial0.printf("[%u] get binary length: %u\n", num, length);
+#endif
+
+    if (length == 0)
+    {
+      break;
+    }
+
+#ifdef ENABLE_DEBUG_PRINT
     hexdump(payload, length);
+#endif
+
+    if (payload[0] == 0x57)
+    {
+      Serial1.write(payload, length);
+    }
 
     break;
+  case WStype_TEXT:
   case WStype_ERROR:
   case WStype_FRAGMENT_TEXT_START:
   case WStype_FRAGMENT_BIN_START:
@@ -62,20 +91,25 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
 
 void setup()
 {
-  Serial.begin(115200);
+#ifdef ENABLE_DEBUG_PRINT
+  Serial0.begin(115200);
 
-  Serial.setDebugOutput(true);
+  Serial0.setDebugOutput(true);
 
-  Serial.println();
-  Serial.println();
-  Serial.println();
+  Serial0.println();
+  Serial0.println();
+  Serial0.println();
+#endif
 
-  for (uint8_t t = 4; t > 0; t--)
-  {
-    Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
-    Serial.flush();
-    delay(1000);
-  }
+  pixels.begin();
+
+  pixels.setPixelColor(0, LED_COLOR_SETUP);
+  pixels.show();
+
+  Serial1.setPins(CH9329_RX_PIN, CH9329_TX_PIN);
+  Serial1.begin(9600);
+
+  delay(1000);
 
   wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
 
@@ -84,11 +118,17 @@ void setup()
     delay(100);
   }
 
-  Serial.print("Connected! IP address: ");
-  Serial.println(WiFi.localIP());
+#ifdef ENABLE_DEBUG_PRINT
+  Serial0.print("Connected! IP address: ");
+  Serial0.println(WiFi.localIP());
+  Serial0.flush();
+#endif
 
   webSocket.begin();
   webSocket.onEvent(webSocketEvent);
+
+  pixels.setPixelColor(0, LED_COLOR_NORMAL);
+  pixels.show();
 }
 
 void loop()
