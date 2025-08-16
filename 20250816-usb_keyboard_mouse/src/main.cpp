6bbaf25b8fa8fdf4770511c4bb89904e939cf6c6
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-08-16 16:13:36 +0900

    update

--- 20250816-usb_keyboard_mouse/src/main.cpp
+++ 20250816-usb_keyboard_mouse/src/main.cpp
@@ -1,23 +1,37 @@
 #include <Arduino.h>
+#include <M5Unified.h>
 
 #include "USB.h"
 #include "USBHIDKeyboard.h"
+#include "USBHIDMouse.h"
 
 USBHID HID;
 USBHIDKeyboard Keyboard;
+USBHIDMouse Mouse;
 
 void setup()
 {
   Serial.begin(115200);
+  M5.begin();
 
   HID.begin();
   USB.begin();
   Keyboard.begin();
+  Mouse.begin();
 }
 
-void loop() {
-  delay(10000);
-  Keyboard.press('a');
-  delay(100);
-  Keyboard.releaseAll();
+void loop()
+{
+  M5.update();
+  if (M5.BtnA.isHolding())
+  {
+    Mouse.move(0, 4);
+    delay(100);
+  }
+  if (M5.BtnA.wasClicked())
+  {
+    Keyboard.press('a');
+    delay(100);
+    Keyboard.releaseAll();
+  }
 }
