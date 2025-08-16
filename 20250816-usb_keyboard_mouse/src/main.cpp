02e9e2e90fadead6c976d5817383caf452fb6ec8
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-08-16 16:07:34 +0900

    keyboard

--- 20250816-usb_keyboard_mouse/src/main.cpp
+++ 20250816-usb_keyboard_mouse/src/main.cpp
@@ -4,20 +4,20 @@
 #include "USBHIDKeyboard.h"
 
 USBHID HID;
-USBHIDKeyboard Keyboard(HID);
+USBHIDKeyboard Keyboard;
 
 void setup()
 {
-  // CDC(Serial) を使いたい場合
   Serial.begin(115200);
 
-  // HID開始→USB開始（順番はどちらでも動きますが、この順がわかりやすい）
   HID.begin();
   USB.begin();
-
-  // 例: キー送信
-  delay(1500); // ホストが列挙し終わるのを待つ
-  Keyboard.print("Hello from AtomS3U!");
+  Keyboard.begin();
 }
 
-void loop() {}
+void loop() {
+  delay(10000);
+  Keyboard.press('a');
+  delay(100);
+  Keyboard.releaseAll();
+}
