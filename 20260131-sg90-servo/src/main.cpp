ac9529eb576d0b3d61ec1aee1c952966a3f30819
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2026-01-31 18:43:11 +0900

    servo

---
+++ 20260131-sg90-servo/src/main.cpp
@@ -0,0 +1,78 @@
+#include <M5Unified.h>
+#include <ESP32Servo.h>
+
+static const int SERVO_PIN1 = 1;
+static const int SERVO_PIN2 = 2;
+static Servo servo1;
+static Servo servo2;
+
+void setup()
+{
+  auto cfg = M5.config();
+  M5.begin(cfg);
+
+  M5.Display.setTextSize(2);
+  M5.Display.println("SG90 Servo test (ESP32Servo)");
+
+  servo1.setPeriodHertz(50);
+  servo1.attach(SERVO_PIN1, 500, 2400);
+
+  servo2.setPeriodHertz(50);
+  servo2.attach(SERVO_PIN2, 500, 2400);
+
+  servo1.write(90); // 中央
+  servo2.write(90); // 中央
+  delay(500);
+}
+
+#define DELAY_MS 40
+
+void loop()
+{
+  M5.update();
+
+  // 90->0->180->0->90
+  for (int a = 90; a >= 0; a -= 5)
+  {
+    servo1.write(a);
+    delay(DELAY_MS);
+  }
+  for (int a = 0; a <= 180; a += 5)
+  {
+    servo1.write(a);
+    delay(DELAY_MS);
+  }
+  for (int a = 180; a >= 0; a -= 5)
+  {
+    servo1.write(a);
+    delay(DELAY_MS);
+  }
+  for (int a = 0; a <= 90; a += 5)
+  {
+    servo1.write(a);
+    delay(DELAY_MS);
+  }
+
+  // 90->0->180->0->90
+  for (int a = 90; a >= 30; a -= 5)
+  {
+    servo2.write(a);
+    delay(DELAY_MS);
+  }
+  for (int a = 30; a <= 120; a += 5)
+  {
+    servo2.write(a);
+    delay(DELAY_MS);
+  }
+  for (int a = 120; a >= 30; a -= 5)
+  {
+    servo2.write(a);
+    delay(DELAY_MS);
+  }
+  for (int a = 30; a <= 90; a += 5)
+  {
+    servo2.write(a);
+    delay(DELAY_MS);
+  }
+
+}
