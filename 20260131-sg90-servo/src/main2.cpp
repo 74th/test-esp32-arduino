1b432ff29b61b6f4aa70fd5b38d42e512a1fc721
Atsushi Morimoto <74th.tech@gmail.com>
2026-03-13 21:38:57 +0900

    ServoEasing

--- 20260131-sg90-servo/src/main2.cpp
+++ 20260131-sg90-servo/src/main2.cpp
@@ -14,6 +14,8 @@ const int SERVO2_PIN = 7;
 static ServoEasing Servo1;
 static ServoEasing Servo2;
 
+#define DELAY_MS 40
+
 namespace test2
 {
     void setup()
@@ -29,11 +31,55 @@ namespace test2
         delay(5000);
     }
 
-#define DELAY_MS 40
-
     void loop()
     {
         M5.update();
+        // 引数1: 角度
+        // 引数2: 秒速角度 度/秒
+        // Servo1.startEaseTo(110, 30);
+
+        // 左向く
+        Servo1.startEaseTo(110, 30);
+
         delay(1000);
+
+        // 右向く
+        Servo1.startEaseTo(70, 60);
+
+        delay(1000);
+
+        // 正面
+        Servo1.startEaseTo(90, 30);
+
+        delay(3000);
+
+        // 下向く
+        Servo2.startEaseTo(100, 30);
+
+        delay(1000);
+
+        // 上向く
+        Servo2.startEaseTo(80, 60);
+
+        delay(1000);
+
+        // 正面
+        Servo2.startEaseTo(90, 30);
+
+        delay(3000);
+
+        // 傾聴
+        Servo2.startEaseTo(80, 100);
+        delay(2000);
+        // 頷く
+        Servo2.startEaseTo(100, 100);
+        delay(300);
+        Servo2.startEaseTo(90, 100);
+        delay(300);
+        Servo2.startEaseTo(100, 100);
+        delay(300);
+        Servo2.startEaseTo(90, 100);
+
+        delay(3000);
     }
 }
